2a1cf603d69970a0b869cd34ab3ffee8723a7a9b
ShankarIdeas <85400367+ShankarIdeas@users.noreply.github.com>
2021-06-18 09:57:39 +0530

    Day4 Assignment 2&3
    Included Region, Subregion and Population

--- tryAjax.js
+++ tryAjax.js
@@ -4,8 +4,8 @@ xhr.onload = function(){
     var data = JSON.parse(this.response);
 //    console.log(data);
     data.forEach(element => {
-        document.write(element.name)
-        document.write("<img src="+element.flag+">");
+        document.write(element.name+ " " + element.region+ " " + element.subregion+ " " + element.population +"<br>");
+        document.write("<img src="+element.flag+"><br>");
     });
   
 };
