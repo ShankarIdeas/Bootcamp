cb6b6f5592d50da3c62350425cb8912a8a126b95
ShankarIdeas <85400367+ShankarIdeas@users.noreply.github.com>
2021-06-18 10:28:05 +0530

    Day4 Assignment 2&3
    Country Names, Region, SubRegion and Flags from a Rest service https://restcountries.eu/rest/v2/all

--- tryAjax.js
+++ tryAjax.js
@@ -1,12 +1,23 @@
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://restcountries.eu/rest/v2/all");
-xhr.onload = function(){
-    var data = JSON.parse(this.response);
-//    console.log(data);
-    data.forEach(element => {
-        document.write(element.name+ " " + element.region+ " " + element.subregion+ " " + element.population +"<br>");
-        document.write("<img src="+element.flag+"><br>");
-    });
-  
+xhr.onload = function () {
+  var data = JSON.parse(this.response);
+  //    console.log(data);
+  data.forEach((element) => {
+    document.write(
+      "<div><center>" +
+        element.name +
+        " " +
+        element.region +
+        " " +
+        element.subregion +
+        " " +
+        element.population +
+        "</center></div>"
+    );
+    document.write(
+      "<div><center><img src=" + element.flag + "></center></div>"
+    );
+  });
 };
 xhr.send();
