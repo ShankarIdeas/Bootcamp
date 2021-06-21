cf9dca02be7cc273ad9febd0507aa43131b88daf
ShankarIdeas <85400367+ShankarIdeas@users.noreply.github.com>
2021-06-21 09:53:15 +0530

    Day5 Assignment1
    Resume in JSON

---
+++ resumeInJson.js
@@ -0,0 +1,10 @@
+resume = {
+    name: "Shankar Raj",
+    DOB : "28/08/1978",
+    experience: '17yrs',
+    underGraduate: "Bsc. Biochemistry",
+    postGraduate: "Master of Computer Applications",
+    otherDegrees: "Master of Business Administration",
+    industry: "Information Technology",
+    skillset: "Java, WebServices, XML, WebMethods, Project Management"
+}
